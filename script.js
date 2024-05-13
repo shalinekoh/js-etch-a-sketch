c1f96534909e870039f795f512593a9196b3edc0
shalinekoh <kohshaline@gmail.com>
2024-05-13 15:10:48 +0800

    Remove magic number

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
+let gridSize = 600;
+
 container = document.querySelector(".container")
 col = document.querySelector(".col")
 submitButton = document.querySelector(".submitButton")
 inputField = document.querySelector(".inputField")
 
-
 submitButton.addEventListener("click", createGrid)
-
+// TODO: Add logic so that it creates a fresh sketch pad everytime submit button is pressed
 function createGrid(){
-    console.log("Here")
     const no_of_square = inputField.value
     for (j=0; j < no_of_square; j++){
         row = document.createElement("div")
@@ -15,7 +15,7 @@ function createGrid(){
         for (i=0; i < no_of_square; i++){
             grid = document.createElement("div")
             grid.classList.add("grid")
-            size = `${(960/no_of_square)}px`
+            size = `${(gridSize/no_of_square)}px`
             grid.style.width = size
             grid.style.height = size
             grid.addEventListener("mouseover", function()
