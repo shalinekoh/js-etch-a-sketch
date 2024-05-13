097b54e9b7bf70f75cdcb67f0ca9767166414045
shalinekoh <kohshaline@gmail.com>
2024-05-13 16:26:44 +0800

    Fix typo

--- script.js
+++ script.js
@@ -10,16 +10,15 @@ submitButton.addEventListener("click", createGrid)
 function createGrid(){
     clearGrid();
     const no_of_square = inputField.value
-    // clearGrid();
     for (j=0; j < no_of_square; j++){
         row = document.createElement("div")
         row.classList.add("row")
         for (i=0; i < no_of_square; i++){
             grid = document.createElement("div")
             grid.classList.add("grid")
-            const size = `${(gridSize/no_of_square)}px`
-            grid.style.width = size
-            grid.style.height = size
+            size = Math.floor(gridSize/no_of_square);
+            grid.style.width = `${(size)}px`
+            grid.style.height = `${(size)}px`
             grid.addEventListener("mouseover", function()
                                     {
                                         console.log("Event")
